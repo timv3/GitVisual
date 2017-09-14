42c00de190e1f784612b96c7937dd4ec061c7314
timv3@yahoo.com <timv3@yaho.com>
2017-09-14 13:40:37 -0700

    added yeah

--- Test/Default.aspx.cs
+++ Test/Default.aspx.cs
@@ -9,7 +9,7 @@ namespace Test
     {
         public void button1Clicked(object sender, EventArgs args)
         {
-            button1.Text = "You clicked me";
+            button1.Text = "You clicked me... Yeah";
         }
     }
 }
