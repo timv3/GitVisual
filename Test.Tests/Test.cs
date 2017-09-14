f9fc072cb4b3ada7fd6a93064ab038d49f775e5b
timv3@yahoo.com <timv3@yaho.com>
2017-09-14 13:38:45 -0700

    Start

---
+++ Test.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Test.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ Test/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace Test
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+        }
+    }
+}
